af2420a5b697dc9803539c74fe9cf06bbe4cee2c
nathanmariedev <nathan.marie.pro@gmail.com>
2023-04-17 12:36:13 +0200

    working

--- backend/src/city/city.controller.ts
+++ backend/src/city/city.controller.ts
@@ -1,38 +1,11 @@
-import { Request } from 'express';
-import {
-  Body,
-  Controller,
-  Get,
-  HttpCode,
-  Post,
-  Put,
-  UseGuards,
-  Param,
-  ParseUUIDPipe,
-  NotFoundException,
-  UseInterceptors,
-  Query,
-  Req,
-  Delete,
-  DefaultValuePipe,
-  NotAcceptableException,
-} from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get, Param, UseInterceptors } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
-  ApiCreatedResponse,
-  ApiQuery,
-  ApiOkResponse,
   ApiUnauthorizedResponse,
   ApiTags,
   ApiNotFoundResponse,
-  ApiNoContentResponse,
-  ApiBearerAuth,
 } from '@nestjs/swagger';
-import { EnumValidatorPipe } from '../common/pipes/enum-validator.pipe';
 import { ResponseInterceptor } from '../response.interceptor';
-import { Metadata } from '../common/classes/metadata.class';
-import { IntValidatorPipe } from '../common/pipes/int-validator.pipe';
 import { City } from './classes/city.class';
 import { CityService } from './city.service';
 
@@ -42,30 +15,28 @@ import { CityService } from './city.service';
 @ApiBadRequestResponse({ description: 'Bad request' })
 @ApiUnauthorizedResponse({ description: 'Not authorized' })
 export class CityController {
-
   constructor(private readonly cityService: CityService) {}
 
   //GET -- Récupérer un tuilisateur grace à 'userId'
   @Get('/:insee')
-  @ApiNotFoundResponse({description : "Not Found"})
-  async findById(@Param('insee') insee:string):Promise<City>{
-    console.log("City?")
-    const city=await this.cityService.findOneById(insee)
-    console.log(city)
-    return city
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  async findById(@Param('insee') insee: string): Promise<City> {
+    console.log('City?');
+    const city = await this.cityService.findOneById(insee);
+    console.log(city);
+    return city;
   }
 
   @Get('/find/:query')
-  async findByQuery(@Param('query') query:string):Promise<City[]>{
-    console.log("City?")
-    const city=await this.cityService.findByQuery(query)
-    console.log(city)
-    return city
+  async findByQuery(@Param('query') query: string): Promise<City[]> {
+    console.log('City?');
+    const city = await this.cityService.findByQuery(query);
+    console.log(city);
+    return city;
   }
   //Récupérer tous les utilisateurs
   /*async returnAllUsers(): Promise<User[]> {
     const users= await this.userService.findAll()
     return users;
   }*/
-
 }
--- backend/src/city/models/city.model.ts
+++ backend/src/city/models/city.model.ts
@@ -4,17 +4,14 @@ import { BasicCrudModel } from '../../common/models/basic-crud.model';
 import { Knex } from 'knex';
 import { promises as fs } from 'fs';
 import * as path from 'path';
-import { classToPlain, plainToClass } from 'class-transformer';
 import { City } from '../classes/city.class';
-import { validate } from 'class-validator';
-import { array } from 'joi';
 import axios from 'axios';
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 const sqlDir = path.join(__dirname, '/sql');
-const API_URL='https://api.meteo-concept.com/api/'
-const API_KEY='1e3804f9ba78545c499d5dd4af53caf47fce3f32dd8d4096e003542c12f37144'
+const API_URL = 'https://api.meteo-concept.com/api/';
+const API_KEY = '1e3804f9ba78545c499d5dd4af53caf47fce3f32dd8d4096e003542c12f37144';
 
 @Injectable()
 export class CityModel extends BasicCrudModel<City> {
@@ -22,31 +19,42 @@ export class CityModel extends BasicCrudModel<City> {
     super(
       pg, // Knex provider instance
       'city', // Nom de la table
-      [
-        'insee',
-        'cp',
-        'name'
-      ], // Liste des colonnes
+      ['insee', 'cp', 'name'], // Liste des colonnes
       City, // Type de classe utilisé pour la transformation via 'plainToClass' dans basicCrudModel
     );
   }
 
   //Récupérer tous les utilisateurs de la BDD
-  async findAll():Promise<City[]>{
+  async findAll(): Promise<City[]> {
     const file = await fs.readFile(`${sqlDir}/findAll.sql`);
     const req = await this.pg.raw(file.toString());
-    return req.rows
+    return req.rows;
   }
 
-  async findOneById(insee:string):Promise<City>{
+  async findOneById(insee: string): Promise<City> {
     const file = await fs.readFile(`${sqlDir}/findById.sql`);
     const req = await this.pg.raw(file.toString(), [insee]);
-    return req.rows
+    if (req.rows.length == 1) {
+      return req.rows[0];
+    }
+    const response = await axios.get(`${API_URL}location/city`, {
+      params: {
+        token: API_KEY,
+        insee: insee,
+      },
+    });
+    const data = response.data.city;
+    const result = new City({ insee: data.insee, cp: data.cp, name: data.name });
+    const addCity = await this.pg.raw(
+      'INSERT INTO "city" ("insee", "cp", "name") VALUES (?, ?, ?);',
+      [result.insee, result.cp, result.name],
+    );
+    return result;
   }
 
-  async findByQuery(query:string):Promise<City[]>{
-    console.log(API_URL)
-    let result:Array<City>
+  async findByQuery(query: string): Promise<City[]> {
+    console.log(API_URL);
+    let result: Array<City>;
     const response = await axios.get(`${API_URL}location/cities`, {
       params: {
         token: API_KEY,
@@ -54,10 +62,13 @@ export class CityModel extends BasicCrudModel<City> {
       },
     });
     const data = response.data.cities;
-    console.log(data.length)
-    result=data.map(city => new City({insee:city.insee,cp:city.cp,name:city.name}) )
-    console.log(result)
-    return result
+    console.log(data.length);
+    // eslint-disable-next-line prefer-const
+    result = data.map(
+      (city: { insee: any; cp: any; name: any }) =>
+        new City({ insee: city.insee, cp: city.cp, name: city.name }),
+    );
+    console.log(result);
+    return result;
   }
-
 }
