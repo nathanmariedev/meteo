14a71b99ed139a718d9c1e7c54496edaf74a1216
nathanmariedev <nathan.marie.pro@gmail.com>
2023-04-19 15:34:43 +0200

    documentation favs

--- backend/src/favs/favs.controller.ts
+++ backend/src/favs/favs.controller.ts
@@ -1,5 +1,10 @@
 import { Controller, Delete, Get, Post, Param, UseInterceptors, Body } from '@nestjs/common';
-import { ApiBadRequestResponse, ApiUnauthorizedResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiUnauthorizedResponse,
+  ApiTags,
+  ApiResponse,
+} from '@nestjs/swagger';
 import { ResponseInterceptor } from '../response.interceptor';
 import { Favs } from './classes/favs.class';
 import { FavsService } from './favs.service';
@@ -15,18 +20,21 @@ export class FavsController {
   constructor(private readonly favsService: FavsService) {}
 
   @Get('/:userId')
+  @ApiResponse({ status: 200, description: `User's favourites found` })
   async findByName(@Param('userId') userName: string): Promise<FindFavs[]> {
     const favs = await this.favsService.findByName(userName);
     return favs;
   }
 
   @Delete('/:userId/:insee')
+  @ApiResponse({ status: 200, description: `User's favourite city successfully deleted` })
   async deleteByName(@Param('userId') userName: string, @Param('insee') insee: string) {
     const favs = await this.favsService.deleteByName(userName, insee);
     favs;
   }
 
   @Post()
+  @ApiResponse({ status: 201, description: `User's favourite city successfully added` })
   async addFav(@Body() userName: string, newFav: CreateFavsDto): Promise<Favs> {
     const favs = await this.favsService.addFav(userName, newFav);
     return favs;
